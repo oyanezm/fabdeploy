be8c655d993c3da132161c7d2d87b9b70374b289
oyanezm <oyanezm@gmail.com>
2012-11-27 21:49:27 -0300

    Fix: added task support

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,12 @@
 import inspect
 import sys
+from fabric.tasks import WrappedCallableTask
 
 # taken from http://www.saltycrane.com/blog/2010/09/class-based-fabric-scripts-metaprogramming-hack/
+def is_method_or_task(member):
+    ""
+    return bool(inspect.ismethod(member) or isinstance(member,WrappedCallableTask))
+
 def class_methods_to_functions(instance,module_name):
     '''
     Utility to take the methods of the instance of a class, instance,
@@ -14,9 +19,8 @@ def class_methods_to_functions(instance,module_name):
 
     # Iterate over the methods of the class and dynamically create a function
     # for each method that calls the method and add it to the current module
-    for method in inspect.getmembers(instance, predicate=inspect.ismethod):
+    for method in inspect.getmembers(instance, predicate=is_method_or_task):
         method_name, method_obj = method
-
         if not method_name.startswith('_'):
             # get the bound method
             func = getattr(instance, method_name)
