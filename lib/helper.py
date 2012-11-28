4bad1c88eaf55fc3a19abb0c1ec219887e4f3dfe
oyanezm <oyanezm@gmail.com>
2012-11-28 02:42:21 -0300

    copy_keys moved from utils

--- lib/helper.py
+++ lib/helper.py
@@ -16,3 +16,8 @@ def _is_host(hosts,hostname):
             return True
     return False
 
+def copy_keys(dict1,dict2):
+    """
+    copy keys from one dictionary to another
+    """
+    for key in dict2.keys(): dict1[key] = dict2[key]
