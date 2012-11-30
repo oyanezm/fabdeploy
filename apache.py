0690a974491131c24c4337f04eb1f8cb9a18b1d8
oyanezm <oyanezm@gmail.com>
2012-11-29 23:26:45 -0300

    env.path references moved to "root_path"

--- apache.py
+++ apache.py
@@ -12,12 +12,12 @@ def configure(self):
         wsgi = env.wsgi_path,
         admin = env.admin,
         server = env.server,
-            root = env.path,
+            root = env.root_path,
             home = env.home,
         template_path = env.conf_template,
         venv_path = ''.join([env.home,'.virtualenvs/',env.project_name]),
-        errorlog = ''.join([env.path,'log/apache_error.log']),
-        accesslog = ''.join([env.path,'log/apache_access.log']),
+        errorlog = ''.join([env.root_path,'log/apache_error.log']),
+        accesslog = ''.join([env.root_path,'log/apache_access.log']),
         )
 
     host.configure(config)
