bfe1e3fa25d92c917037b63606ef1477e9cc11d7
oyanezm <oyanezm@gmail.com>
2013-10-09 19:35:17 -0400

    update apache logs path. update virtualhosts.

--- apache.py
+++ apache.py
@@ -5,29 +5,33 @@ from fabdeploy.servers import get_host
 @task
 def configure(self):
     """
-    configures apache
+    configures apache based on Host
     """
-    host = get_host()
     config = _AttrDict(
         wsgi = env.wsgi_path,
         admin = env.admin,
         server = env.server,
             root = env.root_path,
             home = env.home,
-        template_path = env.conf_template,
+        vhost_path = env.vhost_template,
         venv_path = ''.join([env.home,'.virtualenvs/',env.project_name]),
-        errorlog = ''.join([env.root_path,'log/apache_error.log']),
-        accesslog = ''.join([env.root_path,'log/apache_access.log']),
-        )
+        errorlog  = ''.join([env.log_path,'error.log']),
+        accesslog = ''.join([env.log_path,'access.log']),
+    )
 
+    # get host and set configuration files
+    host = get_host()
     host.configure(config)
+
+    # remove old logs
     with settings(warn_only = True):
-        # remove logs
         run("rm -rf %(errorlog)s %(accesslog)s" % config)
+
     if env.use_sudo:
         sudo("chown root:root %s" % env.vhost_path)
         restart()
-    host.setup()
+
+    host.setup() # ony for alwatsdata TODO: reorganize
 
 def restart(self):
     """
