c96f6022ecacab4ea4059123319aa0585e74d1ef
oyanezm <oyanezm@gmail.com>
2013-10-10 20:04:17 -0400

    adds apache config to sites_available path and adds symbolic link to sites_enabled

--- servers.py
+++ servers.py
@@ -2,7 +2,7 @@ from fabric.api import run, env, put
 from fabric.contrib import files
 from pdb import set_trace as brake
 
-class _BaseHost(object):
+class _BaseServer(object):
     """
     base class that holds safe methods
     """
@@ -25,7 +25,7 @@ class _BaseHost(object):
         pass
 
 
-class _Alwaysdata(_BaseHost):
+class _Alwaysdata(_BaseServer):
     """
     always data config setup
     """
@@ -48,25 +48,42 @@ class _Alwaysdata(_BaseHost):
         fcgi_destiny = '/'.join(env.wsgi_path.split('/')[:-1]+['django.fcgi'])
         run("chmod +x %s" % fcgi_destiny)
 
-class _Localhost(_BaseHost):
+class _Localhost(_BaseServer):
     """
     localhost configuration
     """
     name = "localhost"
 
     def configure(self,config):
+        from fabric.api import settings,sudo,env
         """
         updates apache virtualhosts
         """
+        filename = env.url; # use url as filename e.g foo.bar.com
+        sites_enabled_path = ''.join([env.apache_path,'sites-enabled/',filename]);
+        sites_available_path = ''.join([env.apache_path,'sites-available/',filename]);
+
+        # wipe data logs, virtualhost config
+        with settings(warn_only = True):
+            run("rm -rf %(errorlog)s %(accesslog)s" % config)
+            run("rm -rf %s" % sites_available_path)
+            run("rm -rf %s" % sites_enabled_path)
+
+        # upload file
         files.upload_template(
-            filename = config.vhost_template,
-            destination = env.vhost_path,
+            filename = env.vhost_template,
+            destination = sites_available_path,
             use_sudo = env.use_sudo,
             context = config,
         )
-        #TODO: add symbolic link
 
-def get_host():
+        # set permitions and restart apache
+        if env.use_sudo:
+            sudo("chown root:root %s" % sites_available_path)
+            sudo("ln -s %s %s" % (sites_available_path, sites_enabled_path) )
+            sudo('/etc/init.d/apache2 restart')
+
+def get_server():
     """
     returns the host class
     """
