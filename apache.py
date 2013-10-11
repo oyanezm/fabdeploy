6badf38c0c52995a90de0b677efca86c582f27b3
oyanezm <oyanezm@gmail.com>
2013-10-10 20:09:40 -0400

    logic from adding config moved to server.py module

--- apache.py
+++ apache.py
@@ -1,39 +1,28 @@
 from fabdeploy.lib.utils import _AttrDict, _is_host
-from fabric.api import settings,run,sudo,env,task 
-from fabdeploy.servers import get_host
+from fabric.api import run,sudo,env,task 
+from fabdeploy.servers import get_server
 
 @task
-def configure(self):
+def configure():
     """
     configures apache based on Host
     """
     config = _AttrDict(
         wsgi = env.wsgi_path,
+        home = env.home,
+        root = env.path,
         admin = env.admin,
-        server = env.server,
-            root = env.root_path,
-            home = env.home,
-        vhost_path = env.vhost_template,
+        server = ''.join(['www.',env.url]),
         venv_path = ''.join([env.home,'.virtualenvs/',env.project_name]),
         errorlog  = ''.join([env.log_path,'error.log']),
         accesslog = ''.join([env.log_path,'access.log']),
     )
 
-    # get host and set configuration files
-    host = get_host()
-    host.configure(config)
+    # get server and set configuration files
+    server = get_server()
+    server.configure(config)
 
-    # remove old logs
-    with settings(warn_only = True):
-        run("rm -rf %(errorlog)s %(accesslog)s" % config)
-
-    if env.use_sudo:
-        sudo("chown root:root %s" % env.vhost_path)
-        restart()
-
-    host.setup() # ony for alwatsdata TODO: reorganize
-
-def restart(self):
+def restart():
     """
     restart apache server
     """
